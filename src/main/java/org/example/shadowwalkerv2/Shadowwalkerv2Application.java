d1900c181eaa89769de315c9d03fbb0c53a82ce2
iason <iason.pifeas@gmail.com>
2025-08-18 10:57:11 +0200

    first commit

---
+++ src/main/java/org/example/shadowwalkerv2/Shadowwalkerv2Application.java
@@ -0,0 +1,13 @@
+package org.example.shadowwalkerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shadowwalkerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shadowwalkerv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/shadowwalkerv2/Shadowwalkerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shadowwalkerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shadowwalkerv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
