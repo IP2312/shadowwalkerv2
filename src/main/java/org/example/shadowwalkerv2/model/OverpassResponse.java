5bed0f8b8480289897bb7a8a3a8e3ce366ccb390
iason <iason.pifeas@gmail.com>
2025-08-20 13:53:43 +0200

    findeRout() implementation

---
+++ src/main/java/org/example/shadowwalkerv2/model/OverpassResponse.java
@@ -0,0 +1,9 @@
+package org.example.shadowwalkerv2.model;
+
+import org.example.shadowwalkerv2.dto.OverpassElement;
+
+import java.util.List;
+
+public class OverpassResponse {
+    private List<OverpassElement> elements;
+}
--- src/main/java/org/example/shadowwalkerv2/service/OverpassService.java
+++ src/main/java/org/example/shadowwalkerv2/service/OverpassService.java
@@ -1,7 +1,13 @@
 package org.example.shadowwalkerv2.service;
 
+import org.example.shadowwalkerv2.dto.OverpassElement;
+import org.example.shadowwalkerv2.model.GeoCoordinate;
+import org.example.shadowwalkerv2.model.OverpassResponse;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 @Service
 public class OverpassService {
@@ -10,8 +16,28 @@ public class OverpassService {
     public OverpassService() {
         this.restTemplate =  new RestTemplate();
     }
+    public OverpassResponse loadRouts(GeoCoordinate start, GeoCoordinate goal){
+        return null;
+    }
 
 
+    private OverpassResponse sendQuery(String query) {
+        String url = "https://overpass-api.de/api/interpreter";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+
+        HttpEntity<String> request = new HttpEntity<>(query, headers);
+
+        ResponseEntity<OverpassResponse> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                OverpassResponse.class
+        );
+
+        return response.getBody();
+    }
 
 
 }
